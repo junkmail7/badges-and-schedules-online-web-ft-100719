5bfe9247564fead3077a1b9f506b77d1cd2f3edb
William Pannell <lpann5@gmail.com>
2019-10-13 22:46:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,13 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+  array.each do |name|
+  badges[x-1]="Hello, #{name}! You'll be assigned to room #{x}!"
+  x=x+1
+  end
+end
+
+def assign_rooms(array)
 badges=[]
 x=1
   array.each do |name|
