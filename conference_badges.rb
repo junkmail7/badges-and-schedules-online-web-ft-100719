e5e332e4cd88f3e2a87120c77f1bd120acbb59a0
William Pannell <lpann5@gmail.com>
2019-10-13 22:26:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,9 @@
 # Write your code here.
 badge_maker(array)
+badges=[]
+x=0
 array.each do |name|
-  puts "Stop hitting yourself #{brother}!"
+  x=x+1
+  badges[x] "Hello, #{name}! You'll be assigned to room #{x}!"
+end
 end
